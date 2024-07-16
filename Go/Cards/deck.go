4239550d2cb5b09d6aa34b5dad6d841dee510948
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2024-07-15 21:29:52 -0300

    feat: go-02

--- Go/Cards/deck.go
+++ Go/Cards/deck.go
@@ -1,11 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
+
+		for _, suit := range cardSuits {
+			for _, cardValue := range cardValues {
+				cards = append(cards, cardValue + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ", ")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- Go/Cards/main.go
+++ Go/Cards/main.go
@@ -1,14 +1,7 @@
 package main
 
 func main() {
-	// var card string = "Ace of Spades"
+	cards := newDeck()
 
-	cards := deck{"Ace of Diamonds"}
-	cards = append(cards, newCard())
-
-	cards.print()
-}
-
-func newCard () string {
-	return "Ace of Spades"
+	cards.saveToFile("MyCards.txt")
 }
