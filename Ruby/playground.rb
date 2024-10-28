cac8897afa88801d48d76d9b6e368fb44203ee1d
Gustavo B. Cassalho <hasllow@gmail.com>
2024-10-28 20:08:51 -0300

    feat: ruby-03

--- Ruby/playground.rb
+++ Ruby/playground.rb
@@ -23,17 +23,34 @@ puts 'My name is #{first_name} #{last_name}'
 # first_name = gets.chomp
 # puts "Hello #{first_name}"
 
-puts "Give a number to multiply by 2"
-input = gets.chomp
-puts input.to_i * 2
+# puts "Give a number to multiply by 2"
+# input = gets.chomp
+# puts input.to_i * 2
 
 
-puts "Enter first name"
-f_name = gets.chomp
-puts "Enter last name"
-l_name = gets.chomp
-full_name = "#{f_name} #{l_name}"
+# puts "Enter first name"
+# f_name = gets.chomp
+# puts "Enter last name"
+# l_name = gets.chomp
+# full_name = "#{f_name} #{l_name}"
 
-puts "Your full name is #{full_name}"
-puts "Your full name reversed is #{full_name.reverse}"
-puts "Your full name has #{f_name.length + l_name.length} characters in it"
+# puts "Your full name is #{full_name}"
+# puts "Your full name reversed is #{full_name.reverse}"
+# puts "Your full name has #{f_name.length + l_name.length} characters in it"
+
+puts "I am a line"
+puts "-" * 20
+20.times {print "-"}
+
+puts "Simple calculator"
+25.times {print "-"}
+puts 
+puts "Enter the first number:"
+first_number = gets.chomp
+puts "Enter the second number:"
+second_number = gets.chomp
+puts "The first number * the second number is #{first_number.to_f * second_number.to_f}"
+puts "The first number + the second number is #{first_number.to_f + second_number.to_f}"
+puts "The first number - the second number is #{first_number.to_f - second_number.to_f}"
+puts "The first number / the second number is #{first_number.to_f / second_number.to_f}"
+puts "The first number % the second number is #{first_number.to_f % second_number.to_f}"
