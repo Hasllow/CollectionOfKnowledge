042714a32a1b18096b982c9e1730442cfb08ed27
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2025-02-21 13:58:53 -0300

    feat: go-max-04

--- Go/Maximiliam/investment-calculator/investment-calculator.go
+++ Go/Maximiliam/investment-calculator/investment-calculator.go
@@ -7,9 +7,16 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	years := 10.0
-	expectedReturnRate := 5.5
+	var investmentAmount, years, expectedReturnRate float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years Amount: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate Amount: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
