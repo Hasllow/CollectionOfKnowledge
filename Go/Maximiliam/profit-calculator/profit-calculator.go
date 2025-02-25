5d52ba36bbcdd1bdddb1015d5b4bce5a7053d641
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2025-02-25 15:50:19 -0300

    feat: go-max-08

--- Go/Maximiliam/profit-calculator/profit-calculator.go
+++ Go/Maximiliam/profit-calculator/profit-calculator.go
@@ -5,20 +5,27 @@ import "fmt"
 func main() {
 	var revenue, expenses, tax_rate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	revenue = getUserValue("Revenue: ")
+	expenses = getUserValue("Expenses: ")
+	tax_rate = getUserValue("Tax Rate: ")
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&tax_rate)
-
-	ebt := revenue - expenses
-	eat := ebt * (1 - tax_rate/100)
-	ratio := ebt / eat
+	ebt, eat, ratio := calculateProfit(revenue, expenses, tax_rate)
 
 	fmt.Printf("Earnings Before Tax: %.2f\n", ebt)
 	fmt.Printf("Earnings After Tax: %.2f\n", eat)
 	fmt.Printf("Ratio: %.2f\n", ratio)
+}
+
+func getUserValue(message string) float64 {
+	var value float64
+	fmt.Print(message)
+	fmt.Scan(&value)
+	return value
+}
+
+func calculateProfit(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	eat := ebt * (1 - taxRate/100)
+	ratio := ebt / eat
+	return ebt, eat, ratio
 }
