64041d3cd5adf6dc69babde7fb4891583c7b963d
Gustavo B. Cassalho <g.b.cassalho@gmail.com>
2025-08-22 21:51:06 -0300

    feat: node-max-10

--- Node/Maximiliam/04 - Routes/models/product.js
+++ Node/Maximiliam/04 - Routes/models/product.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const Cart = require("./cart");
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   "data",
@@ -44,6 +46,21 @@ module.exports = class Product {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
+      });
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      const updatedProducts = products.filter((p) => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+        if (err) {
+          console.log(err);
+        }
       });
     });
   }
