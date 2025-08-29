24d307d2af9913b4c093f7e0cd4ea558d7d36be3
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2025-08-29 16:37:52 -0300

    feat: node-max-12

--- Node/Maximiliam/04 - Routes/app.js
+++ Node/Maximiliam/04 - Routes/app.js
@@ -13,10 +13,11 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const e = require("express");
 
-db.execute(
-  "CREATE TABLE IF NOT EXISTS products (id INT UNSIGNED NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, price DOUBLE NOT NULL, description TEXT NOT NULL, imageUrl VARCHAR(255) NOT NULL, PRIMARY KEY (id), UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE)"
-);
+// db.execute(
+//   "CREATE TABLE IF NOT EXISTS products (id INT UNSIGNED NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, price DOUBLE NOT NULL, description TEXT NOT NULL, imageUrl VARCHAR(255) NOT NULL, PRIMARY KEY (id), UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE)"
+// );
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -26,4 +27,8 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(4000);
+
+// db.execute("SELECT * FROM products")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
