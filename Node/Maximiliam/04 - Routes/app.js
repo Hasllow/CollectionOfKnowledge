f13bc959e2578df40ca99e7e0d4be08ed1d58407
Gustavo B. Cassalho <g.b.cassalho@gmail.com>
2025-09-01 21:16:23 -0300

    feat: node-max-13

--- Node/Maximiliam/04 - Routes/app.js
+++ Node/Maximiliam/04 - Routes/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -13,11 +13,6 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const e = require("express");
-
-// db.execute(
-//   "CREATE TABLE IF NOT EXISTS products (id INT UNSIGNED NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, price DOUBLE NOT NULL, description TEXT NOT NULL, imageUrl VARCHAR(255) NOT NULL, PRIMARY KEY (id), UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE)"
-// );
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -27,8 +22,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(4000);
-
-// db.execute("SELECT * FROM products")
-//   .then((data) => console.log(data))
-//   .catch((err) => console.log(err));
+sequelize
+  .sync()
+  .then(() => app.listen(4000))
+  .catch((err) => console.log(err));
--- Node/Maximiliam/04 - Routes/util/database.js
+++ Node/Maximiliam/04 - Routes/util/database.js
@@ -1,11 +1,20 @@
 //docker run --name mysql -e MYSQL_ROOT_PASSWORD= -d -p 3306:3306 mysql:latest
-const mysql2 = require("mysql2");
+// const mysql2 = require("mysql2");
 
-const pool = mysql2.createPool({
+// const pool = mysql2.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node",
+//   password: "1234#",
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node", "root", "1234#", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node",
-  password: "1234#",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
